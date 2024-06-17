4147efeb345a7e71f78fdb922d48d8bbe41aef04
Will Marshall <willcodymarshall@gmail.com>
2024-06-16 22:49:14 -0400

    remove unused import

--- src/main/java/frc/robot/subsystems/drive/Swerve.java
+++ src/main/java/frc/robot/subsystems/drive/Swerve.java
@@ -23,7 +23,6 @@ import edu.wpi.first.math.kinematics.SwerveModulePosition;
 import edu.wpi.first.math.kinematics.SwerveModuleState;
 import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj.ADIS16470_IMU;
-import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
